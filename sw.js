157d516dbda0685ec678f170b132c89cb501ab9f
Yusuf F. Adiputera <yfa.adiputera@gmail.com>
2025-07-11 17:10:35 +0700

    Further enhance SEO
    minify css & JS, add header, main, footer section

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./images/128.png", "./images/192.png", "./images/256.png", "./images/512.png", "./images/astra.svg"]);
+            return cache.addAll(["./", "./images/astra.svg", "./src/master.min.css", "./src/index.min.js"]);
         })
     );
 });
