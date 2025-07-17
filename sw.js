d1b15488aad368125186e2db0f7312c3dfb45b36
Yusuf F. Adiputera <yfa.adiputera@gmail.com>
2025-07-17 17:43:56 +0700

    Add push notification listener, add maskable icon, add badge

--- sw.js
+++ sw.js
@@ -13,3 +13,34 @@ self.addEventListener("fetch", e => {
         })
     );
 });
+
+self.addEventListener('push', function (event) {
+    console.log("Push event received:", event);
+
+    let data = {
+        title: "Hello!",
+        body: "You have a new notification!",
+        icon: "/images/128.png",
+        image: "/images/512.png",
+        badge: "/images/badge.png"
+    };
+
+    if (event.data) {
+        try {
+            const json = event.data.json();
+            data = { ...data, ...json };
+        } catch (e) {
+            console.warn("Push data is not valid JSON, using as text:", e);
+            data.body = event.data.text();
+        }
+    }
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: data.icon,
+            image: data.image,
+            badge: data.badge,
+        })
+    );
+});
