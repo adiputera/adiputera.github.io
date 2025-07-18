f90ec59f1bc901254cc29adcf241f7b5e8b9f03c
Yusuf F. Adiputera <yfa.adiputera@gmail.com>
2025-07-18 13:37:02 +0700

    Update js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
+    navigator.serviceWorker.register("sw.js").then(reg => {
         reg.update();
     }).catch(error => {
         console.error("Service Worker registration failed:", error);
